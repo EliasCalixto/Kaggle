49e538cdaedfcb73a23172200ca265ee8abf6de0
Elias Calixto <85374767+EliasCalixto@users.noreply.github.com>
2022-01-09 14:48:10 -0500

    ss

--- SQL for data scientist/Script-train.sql
+++ SQL for data scientist/Script-train.sql
@@ -17,4 +17,41 @@ FROM dataset_1
 GROUP BY destination, time;
 
 SELECT *
-FROM dataset_1;
+FROM dataset_1 d 
+UNION
+SELECT *
+FROM table_to_union;
+
+SELECT *
+FROM dataset_1 d;
+
+SELECT DISTINCT destination
+FROM
+(
+SELECT *
+FROM dataset_1 d 
+UNION
+SELECT *
+FROM table_to_union);
+
+SELECT *
+FROM table_to_join ttj;
+
+SELECT destination, d.time, ttj.part_of_day
+FROM dataset_1 d 
+LEFT JOIN table_to_join ttj 
+ON d.time = ttj.time 
+
+SELECT *
+FROM dataset_1 d 
+WHERE d.time LIKE '%P%'
+
+SELECT DISTINCT temperature
+FROM dataset_1 d 
+WHERE temperature BETWEEN 29 AND 75;
+
+SELECT occupation 
+FROM dataset_1 d 
+WHERE occupation IN ('Sales & Related', 'Management');
+
+
